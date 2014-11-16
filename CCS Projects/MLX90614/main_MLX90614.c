302ff195b586fbd6e739f736b2f6f898874070d6
Nipun Gunawardena <1.madvoid@gmail.com>
2014-11-16 15:24:00 -0700

    Added working ambient temperature

--- CCS Projects/MLX90614/main_MLX90614.c
+++ CCS Projects/MLX90614/main_MLX90614.c
@@ -113,23 +113,34 @@ int main(void) {
 	uint16_t tempVals = ( ((uint16_t) g_mlxValBytes[1]) << 8 ) | ( (uint16_t) g_mlxValBytes[0] );
 	g_objectTemp = ((float) tempVals) * 0.02 - 273.15;
 
-//	UCB0CTLW0 |= UCTR;				// Change to send
-//	g_mlxRxCount = 0;				// Reset receive count
-//
-//	// Send ambient temperature read command
-//	UCB0CTL1 |= UCTXSTT;			// Send start
-//	while(!(UCB0IFG & UCTXIFG0));	// Wait for TX interrupt flag
-//	UCB0TXBUF = MLX90614_TA;		// Send object temperature command
-//	while(!(UCB0IFG & UCTXIFG0));	// Wait for TX interrupt flag
-//	UCB0CTLW0 &= ~UCTR;				// Change to receive
-//	UCB0CTLW0 |= UCTXSTT;			// Send restart
-//	while(UCB0CTLW0 & UCTXSTT);		// Wait for restart
-//	__bis_SR_register(LPM0_bits);	// Enter low power mode and wait for bytes
-//
-//	g_ambientTemp = 0.0;
-//	tempVals = 0;
-//	tempVals = ( ((uint16_t) g_mlxValBytes[1]) << 8 ) | ( (uint16_t) g_mlxValBytes[0] );
-//	g_ambientTemp = ((float) tempVals) * 0.02 - 273.15;
+//	__delay_cycles(100000);
+
+	g_mlxRxCount = 0;				// Reset receive count
+	// Configure USCI_B0 for I2 mode - Sending
+	UCB0CTLW0 |= UCSWRST;		// Software reset enabled
+	UCB0CTLW0 |= UCMODE_3 | UCMST | UCSYNC | UCTR | UCSSEL__SMCLK; // I2C mode, master, sync, sending, SMCLK
+	UCB0BRW = 0xA;				// Baudrate = SMCLK / 10;
+	UCB0CTLW1 |= UCASTP_2;		// Auto stop
+	UCB0TBCNT = 0x03;			// Auto stop after 3 bytes
+	UCB0I2CSA = MLX90614_I2C_ADDRESS;	// I2C Address
+	UCB0CTL1 &= ~UCSWRST;		// Clear reset
+	UCB0IE |= UCRXIE;			// Set rx interrupt
+	UCB0IE &= ~UCTXIE;
+
+	// Send ambient temperature read command
+	UCB0CTL1 |= UCTXSTT;			// Send start
+	while(!(UCB0IFG & UCTXIFG0));	// Wait for TX interrupt flag
+	UCB0TXBUF = MLX90614_TA;		// Send object temperature command
+	while(!(UCB0IFG & UCTXIFG0));	// Wait for TX interrupt flag
+	UCB0CTLW0 &= ~UCTR;				// Change to receive
+	UCB0CTLW0 |= UCTXSTT;			// Send restart
+	while(UCB0CTLW0 & UCTXSTT);		// Wait for restart
+	__bis_SR_register(LPM0_bits);	// Enter low power mode and wait for bytes
+
+	g_ambientTemp = 0.0;
+	tempVals = 0;
+	tempVals = ( ((uint16_t) g_mlxValBytes[1]) << 8 ) | ( (uint16_t) g_mlxValBytes[0] );
+	g_ambientTemp = ((float) tempVals) * 0.02 - 273.15;
 
 	__no_operation();
 }
