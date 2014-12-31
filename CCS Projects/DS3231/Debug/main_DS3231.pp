e3610898a1facf70dc12b0e5f335557785047fe0
Nipun Gunawardena <1.madvoid@gmail.com>
2014-12-31 03:51:18 -0700

    Added working DS3231 project

---
+++ CCS Projects/DS3231/Debug/main_DS3231.pp
@@ -0,0 +1,15 @@
+# FIXED
+
+main_DS3231.obj: ../main_DS3231.c
+main_DS3231.obj: c:/ti/ccsv6/tools/compiler/msp430_4.3.5/include/stdint.h
+main_DS3231.obj: c:/ti/ccsv6/ccs_base/msp430/include/msp430.h
+main_DS3231.obj: c:/ti/ccsv6/ccs_base/msp430/include/msp430fr5969.h
+main_DS3231.obj: c:/ti/ccsv6/ccs_base/msp430/include/in430.h
+main_DS3231.obj: c:/ti/ccsv6/tools/compiler/msp430_4.3.5/include/intrinsics.h
+
+../main_DS3231.c: 
+c:/ti/ccsv6/tools/compiler/msp430_4.3.5/include/stdint.h: 
+c:/ti/ccsv6/ccs_base/msp430/include/msp430.h: 
+c:/ti/ccsv6/ccs_base/msp430/include/msp430fr5969.h: 
+c:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+c:/ti/ccsv6/tools/compiler/msp430_4.3.5/include/intrinsics.h: 
---
+++ CCS Projects/DS3231/Debug/subdir_vars.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430fr5969.cmd 
+
+C_SRCS += \
+../main_DS3231.c 
+
+OBJS += \
+./main_DS3231.obj 
+
+C_DEPS += \
+./main_DS3231.pp 
+
+C_DEPS__QUOTED += \
+"main_DS3231.pp" 
+
+OBJS__QUOTED += \
+"main_DS3231.obj" 
+
+C_SRCS__QUOTED += \
+"../main_DS3231.c" 
+
+
