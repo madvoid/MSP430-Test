cee28acc2f9075aca6962fa97448604c9a268fd1
Nipun Gunawardena <1.madvoid@gmail.com>
2015-01-29 12:51:38 -0700

    Slightly modified SD project to help with power consumption testing

--- CCS Projects/SD/main_SD.c
+++ CCS Projects/SD/main_SD.c
@@ -34,34 +34,19 @@ int main(void)
 {
 	  WDTCTL = WDTPW | WDTHOLD;       // Stop WDT
 
-	  // Configure GPIO
-	  P1OUT &= ~BIT0;                           // Clear P1.0 output latch
-	  P1DIR |= BIT0;                            // For LED
+	  // Prepare LEDs
+	  P1DIR = 0xFF ^ (BIT1 | BIT3);             // Set all but P1.1, 1.3 to output direction
+	  P1OUT |= BIT0;							// LED On
+	  P4DIR = 0xFF;							  	// P4 output
+	  P4OUT |= BIT6;							// P4.6 LED on
+
+	  // Prepare I2C and clock pins
 	  P1SEL1 |= BIT6 | BIT7;                    // I2C pins
 	  PJSEL0 |= BIT4 | BIT5;					// Set J.4 & J.5 to accept crystal input for ACLK
 
-	  // Disable the GPIO power-on default high-impedance mode to activate
-	  // previously configured port settings
-	  PM5CTL0 &= ~LOCKLPM5;
-
-	  // Clock setup
-	  CSCTL0_H = CSKEY >> 8;                    // Unlock CS registers
-	  CSCTL1 = DCOFSEL_0;                       // Set DCO to 1MHz
-	  CSCTL2 = SELA__LFXTCLK | SELS__DCOCLK | SELM__DCOCLK; // Set ACLK = LFXTCLK; MCLK = DCO
-	  CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;     // Set all dividers to 1
-	  CSCTL4 &= ~LFXTOFF;						// Turn on LFXT
-
-	  // Lock CS registers - Why isn't PUC created?
-	  CSCTL0_H = 0;
-
-	  // Enable interrupts
-	  __bis_SR_register(GIE);	  WDTCTL = WDTPW | WDTHOLD;       // Stop WDT
-
-	  // Configure GPIO
-	  P1OUT &= ~BIT0;                           // Clear P1.0 output latch
-	  P1DIR |= BIT0;                            // For LED
-	  P1SEL1 |= BIT6 | BIT7;                    // I2C pins
-	  PJSEL0 |= BIT4 | BIT5;					// Set J.4 & J.5 to accept crystal input for ACLK
+	  // Prepare P1.1 and P1.3 for switch
+	  P1OUT = BIT1 | BIT3;                      // Pull-up resistor on P1.1, 1.3
+	  P1REN = BIT1 | BIT3;                      // Select pull-up mode for P1.1, 1.3
 
 	  // Disable the GPIO power-on default high-impedance mode to activate
 	  // previously configured port settings
@@ -80,7 +65,10 @@ int main(void)
 	  // Enable interrupts
 	  __bis_SR_register(GIE);
 
-
+	  // Wait for button press
+	  P4OUT |= BIT6;
+	  while(P1IN & BIT1);
+	  P4OUT &= ~BIT6;							// Turn off red LED
 
 	// Mount the SD Card
 	switch( f_mount(&sdVolume, "", 0) ){
@@ -105,20 +93,27 @@ int main(void)
 	}
 
 	// Initialize result variable
-	UINT bw;
+	UINT bw = 0;
 
 	FloatToPrint(testFloat, printValue);
 
+	int i = 0;
 	// Open & write
 	if(f_open(&logfile, "newfile.txt", FA_WRITE | FA_OPEN_ALWAYS) == FR_OK) {	// Open file - If nonexistent, create
 		f_lseek(&logfile, logfile.fsize);					// Move forward by filesize; logfile.fsize+1 is not needed in this application
-		f_printf(&logfile, "%ld.%ld\n", printValue[0], printValue[1]);
+		for(i = 0; i < 25; i++){
+			f_printf(&logfile, "%ld.%ld\n", printValue[0], printValue[1]);
+		}
+		P4OUT |= BIT6;
+		__delay_cycles(1000000);
+		P4OUT &= ~BIT6;
 		f_close(&logfile);							// Close the file
 		if (bw == 11) {
 			P1OUT |= BIT0;
 		}
 	}
 
+	P1OUT |= BIT0;
 	  __no_operation();
 
 	  while(1);
